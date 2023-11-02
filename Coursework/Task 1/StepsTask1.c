3e8c8445ea0b1b12dfc523b6f97a3488816cbd77
Thomas Fleming <146274550+tommfleming@users.noreply.github.com>
2023-11-02 19:21:41 +0000

    [Coursework 1]

--- Coursework/Task 1/StepsTask1.c
+++ Coursework/Task 1/StepsTask1.c
@@ -11,7 +11,23 @@ typedef struct {
 
 // Define any additional variables here
 
+// Length of line to read in
+int buffer_size = 100;
 
+// String holding each line as it's read in
+char line_buffer[100];
+
+char date[11];
+char time[6];
+char steps[10];
+
+int count = 0;
+
+// Array of records, length is an arbitray large number
+FITNESS_DATA dataList[1000]; 
+
+// Instance of the struct
+FITNESS_DATA data;
 
 // This is your helper function. Do not change it in any way.
 // Inputs: character array representing a row; the delimiter character
@@ -44,5 +60,35 @@ void tokeniseRecord(const char *input, const char *delimiter,
 // Complete the main function
 int main() {
 
+    // Open file
+    char* filename = "FitnessData_2023.csv";
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
 
+    // While not end of file
+    while (fgets(line_buffer, buffer_size, file) != NULL) {
+        tokeniseRecord(line_buffer, ",", date, time, steps);
+
+        // Initialise record data with values outputted from tokeniseRecord()
+        strcpy(data.date, date);
+        strcpy(data.time, time);
+        data.steps = atoi(steps);
+
+        // Insert record data into dataList
+        dataList[count] = data;
+
+        count += 1;
+    }
+
+    // Output number of records and first three lines in file
+    printf("Number of records in file: %d\n", count);
+    printf("%s/%s/%d\n", dataList[0].date, dataList[0].time, dataList[0].steps);
+    printf("%s/%s/%d\n", dataList[1].date, dataList[1].time, dataList[1].steps);
+    printf("%s/%s/%d\n", dataList[2].date, dataList[2].time, dataList[2].steps);
+
+    fclose(file);
+    return 0;
 }
